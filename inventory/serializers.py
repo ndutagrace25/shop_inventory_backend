35103a86671f1d41bb3050702920de74ec13d0e4
Grace Nduta <ndutagrace25@gmail.com>
2026-05-14 13:59:22 +0300

    products CRUD

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Category
+from .models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -8,3 +8,55 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ["id", "name", "description", "created_at"]
         read_only_fields = ["id", "created_at"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source="category.name", read_only=True)
+    profit_per_unit = serializers.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        read_only=True
+    )
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "category",
+            "category_name",
+            "name",
+            "sku",
+            "unit_cost",
+            "selling_price",
+            "quantity_on_hand",
+            "reorder_level",
+            "is_active",
+            "profit_per_unit",
+            "is_low_stock",
+            "created_at",
+            "updated_at",
+        ]
+
+        read_only_fields = [
+            "id",
+            "category_name",
+            "profit_per_unit",
+            "is_low_stock",
+            "created_at",
+            "updated_at",
+        ]
+
+    def validate(self, attrs):
+        unit_cost = attrs.get("unit_cost", getattr(self.instance, "unit_cost", None))
+        selling_price = attrs.get(
+            "selling_price",
+            getattr(self.instance, "selling_price", None)
+        )
+
+        if unit_cost is not None and selling_price is not None:
+            if selling_price < unit_cost:
+                raise serializers.ValidationError(
+                    {
+                        "selling_price": "Selling price cannot be lower than unit cost."
+                    }
+                )
+        return attrs
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
 
 class CategoryListCreateView(generics.ListCreateAPIView):
@@ -10,6 +11,63 @@ class CategoryListCreateView(generics.ListCreateAPIView):
 
     search_fields = ["name", "description"]
 
-    ordering_fields= ["name", "description"]
+    ordering_fields = ["name", "description"]
 
     ordering = ["name"]
+
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.select_related("category").all()
+    serializer_class = ProductSerializer
+
+    search_fields = ["name", "sku", "category__name"]
+
+    ordering_fields = [
+        "name",
+        "sku",
+        "selling_price",
+        "unit_cost",
+        "quantity_on_hand",
+        "created_at",
+    ]
+
+    ordering = ["name"]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+
+        return Response(
+            {"message": "Product created successfuly", "data": serializer.data}
+        )
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.select_related("category").all()
+    serializer_class = ProductSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(
+            {"message": "Product updated successfully", "data": serializer.data},
+            status=status.HTTP_200_OK,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        self.perform_destroy(instance)
+
+        return Response(
+            {"message": "Product deleted successfully"},
+            status=status.HTTP_200_OK,
+        )
